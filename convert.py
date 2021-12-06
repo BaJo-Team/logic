8c6dec594b0c7e3a11002c991f249053ce627580
sunshinetoyou <victorr0618@naver.com>
2021-12-06 10:15:53 +0900

    재업로드

--- convert.py
+++ convert.py
@@ -54,14 +54,54 @@ import win32gui
 #     application.Quit()
 
 # ppt 단일 파일 변환
-output_folder_path = "C:\\Users\\yeonsu\\Desktop\\단국대\\2021 1학년-2학기\\과제\\대학기초SW입문\\팀프로젝트"    # 저장할 폴더 선택 기능 필요
 
-input_file_path = "C:\\Users\\yeonsu\\Desktop\\단국대\\2021 1학년-2학기\\과제\\대학기초SW입문\\팀프로젝트\\test.pptx"
-input_file_name = input_file_path.split("\\")[-1]
+# output_folder_path = "C:\\Users\\yeonsu\\Desktop\\단국대\\2021 1학년-2학기\\과제\\대학기초SW입문\\팀프로젝트"    # 저장할 폴더 선택 기능 필요
+#
+# input_file_path = "C:\\Users\\yeonsu\\Desktop\\단국대\\2021 1학년-2학기\\과제\\대학기초SW입문\\팀프로젝트\\test.pptx"
+# input_file_name = input_file_path.split("\\")[-1]
+#
+# application = win32.Dispatch("Powerpoint.Application")
+#
+# presentation = application.Presentations.Open(input_file_path, ReadOnly=False)
+# presentation.SaveAs(os.path.join(output_folder_path, input_file_name.split(".")[0] + ".pdf"), 32)
+#
+# application.Quit()
 
-application = win32.Dispatch("Powerpoint.Application")
+# word 단일 파일 변환
+# input_file_path = "C:\\Users\\yeonsu\\Desktop\\단국대\\2021 1학년-2학기\\과제\\대학기초SW입문\\팀프로젝트\\test3.docx"  # 파일 절대 경로
+# input_file_name = input_file_path.split("\\")[-1]
+# output_folder_path = os.path.join("C:\\Users\\yeonsu\\Desktop\\단국대\\2021 1학년-2학기\\과제\\대학기초SW입문\\팀프로젝트", input_file_name.split(".")[0] + ".pdf")   # 저장할 폴더 절대 경로
+# print(output_folder_path)
+# file = open(output_folder_path, "w")
+# file.close()
+#
+# application = win32.Dispatch('Word.Application')
+# doc = application.Documents.Open(input_file_path)
+# doc.SaveAs(output_folder_path, 17)
+# doc.Close()
+# application.Quit()
 
-presentation = application.Presentations.Open(input_file_path, ReadOnly=False)
-presentation.SaveAs(os.path.join(output_folder_path, input_file_name.split(".")[0] + ".pdf"), 32)
+#워드 다중파일 변환
+input_folder_path = "C:\\Users\\yeonsu\\Desktop\\단국대\\2021 1학년-2학기\\과제\\대학기초SW입문\\팀프로젝트"  # 파일이 있는 폴더 절대 경로
+input_file_paths = os.listdir(input_folder_path)
 
-application.Quit()
+for input_file_name in input_file_paths:
+    print(input_file_name)
+    if not input_file_name.lower().endswith((".docx", ".doc")):
+        continue
+
+    if input_file_name.lower().startswith("~$"):
+        continue
+
+    output_folder_path = os.path.join("C:\\Users\\yeonsu\\Desktop\\단국대\\2021 1학년-2학기\\과제\\대학기초SW입문\\팀프로젝트", input_file_name.split(".")[0] + ".pdf")  # 저장을 할 폴더 절대 경로
+
+    file = open(output_folder_path, "w")
+    file.close()
+
+    input_file_path = os.path.join(input_folder_path, input_file_name)
+
+    application = win32.Dispatch('Word.Application')
+    doc = application.Documents.Open(input_file_path)
+    doc.SaveAs(output_folder_path, 17)
+    doc.Close()
+    application.Quit()
